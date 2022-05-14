0c1f9ce2f0e9297f0b6f95fdcebcc6620403fb43
wen <wifecooky@gmail.com>
2022-05-14 21:54:23 +0900

    fixed ci/cd

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git config --global user.email "shiori.ou@gmail.com"
 git init
 git add -A
 git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
+git push -f $githubUrl main:gh-pages # 推送到github gh-pages分支
 
 # deploy to coding pages
 # echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
